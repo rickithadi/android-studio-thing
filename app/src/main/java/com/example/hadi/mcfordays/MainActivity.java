0ea32a7a34f9a7dc7a2b6f39b307ba25b04d76e8
rickithadi <rickithadi@gmail.com>
2017-11-08 22:28:40 +0800

    calculator u pand ran

--- app/src/main/java/com/example/hadi/mcfordays/MainActivity.java
+++ app/src/main/java/com/example/hadi/mcfordays/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.hadi.mcfordays;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button butt= (Button) findViewById(R.id.button);
+        butt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText num1= (EditText) findViewById(R.id.firstNum);
+                EditText num2= (EditText) findViewById(R.id.secondNum);
+                TextView result=(TextView) findViewById(R.id.result);
+
+                int number1 = Integer.parseInt(num1.getText().toString());
+                int number2 = Integer.parseInt(num2.getText().toString());
+                int resultz= number1+number2;
+                result.setText(resultz +"");
+            }
+        });
     }
 }
